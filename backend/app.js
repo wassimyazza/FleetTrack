b09ea1637fb335f337cc6faab15171e3b0efb30b
wassim <wassimwassim.ka1@gmail.com>
2025-12-14 22:52:30 +0100

    feat: create login page by consuming the backend APIs

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import path from "path";
-import { fileURLToPath } from "url";
+import cors from "cors";
 import router from "./routes/web.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-dotenv.config();
-
 connectDB();
 
-app.use('/api/',router);
+app.use('/api', router);
 
-
+app.use(errorHandler);
 
 export default app;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:3011/api';
 
 const api = axios.create({
     baseURL: API_URL,
